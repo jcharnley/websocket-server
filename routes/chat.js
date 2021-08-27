daf3d4c41910a9e029f7164fb611b78b61138c12
Jason Charnley <jason.charnley@LDNML01CHV2W.broadband>
2021-08-27 10:10:56 +0100

    added edits changing laptops

--- routes/chat.js
+++ routes/chat.js
@@ -1,8 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var util = require('../util');
+var util = require("../util");
 
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 wss = new WebSocket.Server({ port: 8090, clientTracking: true });
 
 let msgHistory = [];
@@ -11,145 +11,146 @@ let privateRoomMsg = [];
 let fromClient;
 let toClient;
 
-wss.on('connection', (ws, req) => {
-	ws.send('{ "connection" : "ok"}');
-	ws.id = util.uuidc();
+wss.on("connection", (ws, req) => {
+  ws.send('{ "connection" : "ok"}');
+  ws.id = util.uuidc();
 
-	ws.on('message', async (data) => {
-		console.log("data",data)
-		const message = JSON.parse(data);
-	
+  ws.on("message", async (data) => {
+    console.log("data", data);
+    const message = JSON.parse(data);
 
-		switch (message.type) {
-			case 'set_username':
-				const { setName } = message;
-				ws.username = setName;
-				connections.push({ id: ws.id, username: setName });
+    switch (message.type) {
+      case "set_username":
+        const { setName } = message;
+        ws.username = setName;
 
-				wss.clients.forEach(function each(client) {
-					if (client.readyState === WebSocket.OPEN) {
-						client.send(JSON.stringify({ type: 'connections', connections: connections }));
-					}
-				});
-				break;
-			case 'message':
-				msgHistory.push({
-					type: 'history',
-					name: message.name,
-					message: message.message,
-					timeStamp: message.timeStamp,
-				});
-				wss.clients.forEach(function each(client) {
-					if (client.readyState === WebSocket.OPEN) {
-						client.send(JSON.stringify(message));
-					}
-				});
-				break;
-			case 'private_messages_start':
+        connections.push({ id: ws.id, username: setName });
+        console.log("connections", connections);
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({ type: "connections", connections: connections })
+            );
+          }
+        });
+        break;
+      case "message":
+        msgHistory.push({
+          type: "history",
+          name: message.name,
+          message: message.message,
+          timeStamp: message.timeStamp,
+        });
+        wss.clients.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(message));
+          }
+        });
+        break;
+      case "private_messages_start":
+        console.log("privateRoomMsg", privateRoomMsg);
+        // if it alreadys exisits then dont add to roomlist
 
-			console.log("privateRoomMsg",privateRoomMsg)
-// if it alreadys exisits then dont add to roomlist
-			if (privateRoomMsg.find()) {
+        let privateRoomID = util.uuidc();
 
-			}
-				let privateRoomID = util.uuidc();
+        fromClient = await findClientById(ws.id);
+        toClient = await findClientById(message.targetId);
 
-				fromClient = await findClientById(ws.id);
-				toClient = await findClientById(message.targetId);
+        privateRoomMsg.push({
+          type: "private_room_created",
+          targetId: message.targetId,
+          privateRoomID: privateRoomID,
+          privateRoomActive: message.privateRoomActive,
+          privateConnnections: [fromClient, toClient],
+          fromClient: fromClient.id,
+          toClient: toClient.id,
+          messages: [],
+        });
 
-				privateRoomMsg.push({
-					type: 'private_room_created',
-					targetId: message.targetId,
-					privateRoomID: privateRoomID,
-					privateRoomActive: message.privateRoomActive,
-					privateConnnections: [fromClient, toClient],
-					fromClient: fromClient.id,
-					toClient: toClient.id,
-					messages: [],
-				});
+        toClient.send(
+          JSON.stringify({
+            id: message.id,
+            type: "private_room_created",
+            fromClient: fromClient.id,
+            toClient: toClient.id,
+            targetId: message.targetId,
+            targetName: fromClient.username,
+            privateRoomActive: message.privateRoomActive,
+            privateRoomID: privateRoomID,
+          })
+        );
 
-				toClient.send(
-					JSON.stringify({
-						id: message.id,
-						type: 'private_room_created',
-						fromClient: fromClient.id,
-						toClient: toClient.id,
-						targetId: message.targetId,
-						targetName: fromClient.username,
-						privateRoomActive: message.privateRoomActive,
-						privateRoomID: privateRoomID,
-					})
-				);
+        fromClient.send(
+          JSON.stringify({
+            id: message.id,
+            type: "private_room_created",
+            fromClient: fromClient.id,
+            toClient: toClient.id,
+            targetId: ws.id,
+            targetName: message.targetName,
+            privateRoomActive: message.privateRoomActive,
+            privateRoomID: privateRoomID,
+          })
+        );
+        break;
+      case "private_message_room":
+        const privateRoomFound = privateRoomMsg.find((obj) => {
+          if (obj.privateRoomID === message.privateRoomID) {
+            return obj;
+          }
+        });
 
-				fromClient.send(
-					JSON.stringify({
-						id: message.id,
-						type: 'private_room_created',
-						fromClient: fromClient.id,
-						toClient: toClient.id,
-						targetId: ws.id,
-						targetName: message.targetName,
-						privateRoomActive: message.privateRoomActive,
-						privateRoomID: privateRoomID,
-					})
-				);
-				break;
-			case 'private_message_room':
-				const privateRoomFound = privateRoomMsg.find((obj) => {
-					if (obj.privateRoomID === message.privateRoomID) {
-						return obj;
-					}
-				});
+        if (privateRoomFound) {
+          privateRoomFound.messages.push({
+            id: message.id,
+            name: message.name,
+            message: message.message,
+            timeStamp: message.timeStamp,
+            privateRoomID: message.privateRoomID,
+          });
+        }
 
-				if (privateRoomFound) {
-					privateRoomFound.messages.push({
-						id: message.id,
-						name: message.name,
-						message: message.message,
-						timeStamp: message.timeStamp,
-						privateRoomID: message.privateRoomID,
-					});
-				}
+        privateRoomFound.privateConnnections.forEach(function each(client) {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(
+              JSON.stringify({
+                type: "private_message_room",
+                privateRoomMsg: privateRoomFound.messages,
+              })
+            );
+          }
+        });
+        break;
+    }
+  });
 
-				privateRoomFound.privateConnnections.forEach(function each(client) {
-					if (client.readyState === WebSocket.OPEN) {
-						client.send(
-							JSON.stringify({
-								type: 'private_message_room',
-								privateRoomMsg: privateRoomFound.messages,
-							})
-						);
-					}
-				});
-				break;
-		}
-	});
-
-	ws.on('close', (ws, req) => {
-		// console.log(new Date() + ' Peer ' + id + ' disconnected.');
-		const findIndex = connections.findIndex((obj) => obj.id === ws.id);
-		connections.splice(findIndex, 1);
-		wss.clients.forEach(function each(client) {
-			if (client.readyState === WebSocket.OPEN) {
-				client.send(JSON.stringify({ type: 'connections', connections: connections }));
-			}
-		});
-	});
+  ws.on("close", (ws, req) => {
+    // console.log(new Date() + ' Peer ' + id + ' disconnected.');
+    const findIndex = connections.findIndex((obj) => obj.id === ws.id);
+    connections.splice(findIndex, 1);
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({ type: "connections", connections: connections })
+        );
+      }
+    });
+  });
 });
 
 async function findClientById(id) {
-	let clientFound;
-	wss.clients.forEach((client) => {
-		if (client.id === id && client.readyState === WebSocket.OPEN) {
-			clientFound = client;
-		}
-	});
+  let clientFound;
+  wss.clients.forEach((client) => {
+    if (client.id === id && client.readyState === WebSocket.OPEN) {
+      clientFound = client;
+    }
+  });
 
-	return clientFound;
+  return clientFound;
 }
 
-router.get('/', (req, res, next) => {
-	res.status(200).send(msgHistory);
+router.get("/", (req, res, next) => {
+  res.status(200).send(msgHistory);
 });
 
 module.exports = router;
